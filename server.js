db621edfe3e97ae61e6199346df4fa952d8ce375
DashkevichAS <antonsnov0604@gmail.com>
2022-12-12 16:12:33 +0300

    Delete & Update Requests

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const path = require('path');
 var morgan = require('morgan');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override');
+
 const Post = require('./models/post');
 const Contact = require('./models/contact');
 
@@ -40,6 +42,8 @@ app.use(express.urlencoded({ extended: false }));
 // чтобы был доступ к стилям на сервере и подлючению их в html(ejs)
 app.use(express.static('styles'));
 
+app.use(methodOverride('_method'));
+
 app.get('/', (req, res) => {
   const title = 'Home';
 
@@ -60,17 +64,6 @@ app.get('/contacts', (req, res) => {
     });
 });
 
-app.get('/posts/:id', (req, res) => {
-  const title = 'Post';
-
-  Post.findById(req.params.id)
-    .then((post) => res.render(createPath('post'), { title, post }))
-    .catch((err) => {
-      console.log(err);
-      res.render(createPath('error', { title: 'Error' }));
-    });
-});
-
 app.get('/posts', (req, res) => {
   const title = 'Posts';
   Post.find()
@@ -79,6 +72,49 @@ app.get('/posts', (req, res) => {
     .catch((err) => {
       console.log(err);
       res.render(createPath('error', { title: 'Error' }));
+    });
+});
+
+app.get('/posts/:id', (req, res) => {
+  const title = 'Post';
+  Post.findById(req.params.id)
+    .then((post) => res.render(createPath('post'), { title, post }))
+    .catch((err) => {
+      console.log(err);
+      res.render(createPath('error', { title: 'Error' }));
+    });
+});
+
+app.delete('/posts/:id', (req, res) => {
+  const title = 'Post';
+  Post.findByIdAndDelete(req.params.id)
+    .then((result) => {
+      res.status = 200;
+    })
+    .catch((err) => {
+      console.log(err);
+      res.render(createPath('error', { title: 'Error' }));
+    });
+});
+
+app.get('/edit/:id', (req, res) => {
+  const title = 'Edit Post';
+  Post.findById(req.params.id)
+    .then((post) => res.render(createPath('edit-post'), { post, title }))
+    .catch((error) => {
+      console.log(error);
+      res.render(createPath('error'), { title: 'Error' });
+    });
+});
+
+app.put('/edit/:id', (req, res) => {
+  const { title, author, text } = req.body;
+  const { id } = req.params;
+  Post.findByIdAndUpdate(id, { title, author, text })
+    .then((result) => res.redirect(`/posts/${id}`))
+    .catch((error) => {
+      console.log(error);
+      res.render(createPath('error'), { title: 'Error' });
     });
 });
 
