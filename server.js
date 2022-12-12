ef56ee0b002941796486e2e17e001976bf91da93
DashkevichAS <antonsnov0604@gmail.com>
2022-12-12 11:39:56 +0300

    send new post to MongoDB

--- server.js
+++ server.js
@@ -1,13 +1,25 @@
 const express = require('express');
 const path = require('path');
 var morgan = require('morgan');
-const { urlencoded } = require('express');
+const mongoose = require('mongoose');
+const Post = require('./models/post');
 
 const app = express();
 
 app.set('view engine', 'ejs');
 
 const PORT = 3000;
+
+const db =
+  'mongodb+srv://DashkevichAS:Citroenc5@cluster0.nffmidu.mongodb.net/node-blog?retryWrites=true&w=majority';
+
+mongoose
+  .connect(db, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((res) => console.log('connected to DB'))
+  .catch((error) => console.log(error));
 
 const createPath = (page) =>
   path.resolve(__dirname, 'ejs-views', `${page}.ejs`);
@@ -76,14 +88,15 @@ app.get('/posts', (req, res) => {
 
 app.post('/add-post', (req, res) => {
   const { title, author, text } = req.body;
-  const post = {
-    id: new Date(),
-    date: new Date().toLocaleDateString(),
-    title,
-    author,
-    text,
-  };
-  res.render(createPath('post'), { post, title });
+  const post = new Post({ title, author, text });
+
+  post
+    .save()
+    .then((result) => res.send(result))
+    .catch((err) => {
+      console.log(err);
+      res.render(createPath('error', { title: 'Error' }));
+    });
 });
 
 app.get('/add-post', (req, res) => {
